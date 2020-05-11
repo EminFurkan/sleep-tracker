3242cf0fcd7dce86adff15cbbe1cfabdb9f13157
Furkan Tezeren <emintezeren@gmail.com>
2020-05-11 19:31:45 +0300

    Built loader component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './styles/App.scss';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Landing } from './components/layout/Landing';
 import { Login } from './components/layout/Login';
+import { Loader } from './components/layout/Loader';
 
 export const App = () => {
   return (
@@ -10,6 +11,7 @@ export const App = () => {
       <Route exact path="/" component={Landing} />
       <Switch>
         <Route exact path="/login" component={Login} />
+        <Route exact path="/loader" component={Loader} />
       </Switch>
     </Router>
   );
---
+++ frontend/src/components/layout/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Loader = () => (
+  <div className="loader">
+    <div class="coffee-mug">
+      <div class="coffee-container">
+        <div class="coffee"></div>
+      </div>
+    </div>
+      <span>Loading<span className="dot"></span></span>
+  </div>
+);
