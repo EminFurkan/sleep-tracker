cb19ffc13d1bb6b449b52ae195a1a8c8a60db0cc
Furkan Tezeren <emintezeren@gmail.com>
2020-05-13 18:05:17 +0300

    built auth context

---
+++ frontend/src/context/auth.js
@@ -0,0 +1,15 @@
+import React, { createContext, useContext, useState } from 'react';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuthValue = () => useContext(AuthContext);
