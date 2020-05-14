7db9237ee1ce6230aac3c098023c50884210ff0c
Furkan Tezeren <emintezeren@gmail.com>
2020-05-14 14:26:18 +0300

    Added logout link & functionality to navbar if/when authenticated

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useConextValue, useContextValue } from '../../context/GlobalState';
 
 export const Navbar = () => {
+  const { isAuthenticated, logout, loading } = useContextValue();
+
   return (
     <nav>
       <div className="container">
         <span>
           <h2>SleepTracker</h2>
         </span>
-        <ul>
-          <li>
-            <button>How it works</button>
-          </li>
-          <li>
-            <button>Sign up</button>
-          </li>
-          <li>
-            <Link to="/login">
-              <button>Login</button>
-            </Link>
-          </li>
-        </ul>
+        {!loading && isAuthenticated ? (
+          <ul>
+            <li>
+              <button onClick={logout}>Logout</button>
+            </li>
+          </ul>
+        ) : (
+          <ul>
+            <li>
+              <button>How it works</button>
+            </li>
+            <li>
+              <button>Sign up</button>
+            </li>
+            <li>
+              <Link to="/login">
+                <button>Login</button>
+              </Link>
+            </li>
+          </ul>
+        )}
       </div>
     </nav>
   );
