c0500a8937c92b19b0786392da56e252617f3a9e
Furkan Tezeren <emintezeren@gmail.com>
2020-05-15 16:17:22 +0300

    Fixed redux store to persist state and watch localstorage changes

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+import setAuthToken from './utils/setAuthToken';
 
 const initialState = {};
 
@@ -14,5 +15,14 @@ const store = createStore(
 );
 
 // subscribe to watch localstorage here
+let currentState = store.getState();
+
+store.subscribe(() => {
+  let previousState = currentState;
+  if (previousState.auth.token !== currentState.auth.token) {
+    const token = currentState.auth.token;
+    setAuthToken(token);
+  }
+});
 
 export default store;
