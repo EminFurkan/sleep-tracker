1a268702d15df468cf6cc437a75a1560825d99ac
Furkan Tezeren <emintezeren@gmail.com>
2020-05-15 12:58:18 +0300

    Completed auth reducer

---
+++ frontend/src/reducers/auth.js
@@ -0,0 +1,53 @@
+import {
+  REGISTER_SUCCESS,
+  LOGIN_SUCCESS,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGOUT
+} from '../actions/types';
+
+const initialState = {
+  token: localStorage.getItem('token'),
+  isAuthenticated: null,
+  loading: true,
+  user: null
+};
+
+export default function (state = initialState, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case REGISTER_SUCCESS:
+      return {
+        ...state,
+        ...payload,
+        isAuthenticated: true,
+        loading: false
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        ...payload,
+        isAuthenticated: true,
+        loading: false
+      };
+    case USER_LOADED:
+      return {
+        ...state,
+        ...payload,
+        isAuthenticated: true,
+        loading: false
+      };
+    case AUTH_ERROR:
+    case LOGOUT:
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false,
+        user: null
+      };
+    default:
+      return state;
+  }
+}
