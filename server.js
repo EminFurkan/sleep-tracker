2fb61ec2cc3ad910b33e6b1b232443e45254e2ce
Furkan Tezeren <emintezeren@gmail.com>
2020-06-08 12:14:18 +0300

    Fixed env vars & path to frontend build

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use('/api/event', require('./routes/api/event'));
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('/frontend/build'));
+  app.use(express.static('frontend/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
